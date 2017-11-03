b500686433d5a16adc20a3ebae7286d8e4fa55ea
Sameer Rastogi <sameer.rastogi@gmail.com>
2017-11-03 15:24:35 +0530

    modified:   src/main/java/com/practice/exptrack/controller/ExpenseController.java

--- src/main/java/com/practice/exptrack/controller/ExpenseController.java
+++ src/main/java/com/practice/exptrack/controller/ExpenseController.java
@@ -1,14 +1,24 @@
 package com.practice.exptrack.controller;
 
+import com.practice.exptrack.domain.Expense;
 import com.practice.exptrack.repository.ExpenseRepository;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/expenses")
+@RequestMapping("/expense-tracker")
 public class ExpenseController {
-    private ExpenseRepository repositoryl;
-    public ExpenseController(ExpenseRepository repos){
-        this.repository;
+    ExpenseRepository expenseRepository = new ExpenseRepository();
+    @PutMapping("{id}")
+    public ResponseEntity<Expense> update(@RequestBody long id, @RequestBody Expense expense) {
+
+        Expense expenseEntry = expenseRepository.update(id, expense);
+        if (expenseEntry != null) {
+            return new ResponseEntity<>(expenseEntry, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
+
 }
